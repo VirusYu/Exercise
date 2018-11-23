2a22274a27ca1b47a15655d1df5e743f744d8e72
yucx <qaq!@#123>
2018-11-23 17:59:45 +0800

    将修改源文件修改为promise方法

--- node-demo/splitMap.js
+++ node-demo/splitMap.js
@@ -1,26 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const fileUrl = './test.1.json'
+const file = path.join(__dirname, fileUrl);
+let jsonStr = null;
 
 // 写入文件
-function writeJSONFile(url) {
-  let file = path.join(__dirname, url);
+function writeJSONFile() {
   // 读取json文件并获取数据
   fs.readFile(file, 'utf8', (err, data) => {
     let json = JSON.parse(data);
     let townList = json['features'];
     loopSplitList(townList);
-    let jsonStr = JSON.stringify(json);
-    fs.writeFile(fileUrl, jsonStr, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('文件修改成功，地址：' + file);
+    jsonStr = JSON.stringify(json);
+    let edit = new Promise(editMainFile);
+    edit.then((result) => {
+      console.log(result);
     });
-    console.log('文件已保存！');
+    edit.catch((reason) => {
+      console.log(reason);
+    });
   });
 }
 
+// 修改主文件
+function editMainFile(resolve, reject) {
+  fs.writeFile(fileUrl, jsonStr, function (err) {
+    if (err) {
+      return reject(err);
+    }
+    resolve('文件已保存');
+  });
+}
 // 拆分数据
 function loopSplitList(arr) {
   fs.mkdir('./town', {
