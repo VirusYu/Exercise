681dcb8086adb762ca004d6859cea0a65b99a747
ergou <ergou@uxin.com>
2020-12-23 11:47:40 +0800

    格式化代码

--- node-demo/splitMap.js
+++ node-demo/splitMap.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const fileUrl = './卢龙.json'
+const fileUrl = "./卢龙.json";
 const file = path.join(__dirname, fileUrl);
 let jsonStr = null;
 
 // 写入文件
 function writeJSONFile() {
   // 读取json文件并获取数据
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, "utf8", (err, data) => {
     let json = JSON.parse(data);
-    let townList = json['features'];
+    let townList = json["features"];
     loopSplitList(townList);
     jsonStr = JSON.stringify(json);
     let edit = new Promise(editMainFile);
@@ -29,30 +29,32 @@ function editMainFile(resolve, reject) {
     if (err) {
       return reject(err);
     }
-    resolve('文件已保存');
+    resolve("文件已保存");
   });
 }
 
 // 拆分数据
 function loopSplitList(arr) {
-  fs.mkdir('./town', {
-    recursive: true
-  }, (err) => {
-    if (err) throw err;
-  });
-  arr.forEach(item => {
-    let newFile = item['properties']['admin_id'] || item['properties']['name'];
-    if (item['properties']['admin_id']) {
-      delete item['properties']['admin_id'];
+  fs.mkdir(
+    "./town",
+    {
+      recursive: true,
+    },
+    (err) => {
+      if (err) throw err;
     }
-    item['properties']['cp'] = eval(item['properties']['cp']);
+  );
+  arr.forEach((item) => {
+    let newFile = item["properties"]["admin_id"] || item["properties"]["name"];
+    if (item["properties"]["admin_id"]) {
+      delete item["properties"]["admin_id"];
+    }
+    item["properties"]["cp"] = eval(item["properties"]["cp"]);
     let fileJSON = {
-      "type": "FeatureCollection",
-      "features": [
-        item
-      ],
-      "UTF8Encoding": true
-    }
+      type: "FeatureCollection",
+      features: [item],
+      UTF8Encoding: true,
+    };
     fileJSON = JSON.stringify(fileJSON);
     fs.writeFile(`./town/${newFile}.json`, fileJSON, (err) => {
       if (err) throw err;
